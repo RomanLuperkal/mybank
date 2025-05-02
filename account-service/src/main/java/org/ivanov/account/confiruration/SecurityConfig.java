2594f83be3f6bc3d7cbe0382e1fcd496d1dcec96
Roman <madara-09@bk.ru>
2025-05-03 01:23:38 +0300

    feat: попытка в безопасность

--- account-service/src/main/java/org/ivanov/account/confiruration/SecurityConfig.java
+++ account-service/src/main/java/org/ivanov/account/confiruration/SecurityConfig.java
@@ -6,9 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -17,9 +23,27 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
-                );
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(customizer -> customizer
+                        .jwt(jwtCustomizer -> {
+                            JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+                            jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+                                Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+                                Map<String, Object> account = (Map<String, Object>) resourceAccess.get("account");
+                                List<String> roles = (List<String>) account.get("roles");
 
+                                return roles.stream()
+                                        .map(SimpleGrantedAuthority::new)
+                                        .map(GrantedAuthority.class::cast)
+                                        .toList();
+                            });
+
+                            jwtCustomizer.jwtAuthenticationConverter(jwtAuthenticationConverter);
+                        })
+                );
         return http.build();
     }
 
