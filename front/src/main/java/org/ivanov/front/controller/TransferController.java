1651ec702739b72ddb5f00d90ca2096a9735171a
Roman <madara-09@bk.ru>
2025-06-10 18:20:37 +0300

    feat: наработки transfer-service(5)

--- front/src/main/java/org/ivanov/front/controller/TransferController.java
+++ front/src/main/java/org/ivanov/front/controller/TransferController.java
@@ -1,11 +1,16 @@
 package org.ivanov.front.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.ivanov.front.configuration.security.AccountUserDetails;
+import org.ivanov.front.service.AccountService;
 import org.ivanov.front.service.TransferService;
 import org.ivanov.transferdto.ResponseTransferDto;
 import org.ivanov.transferdto.innertransferdto.InnerTransferReqDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class TransferController {
     private final TransferService transferService;
+    private final AccountService accountService;
 
     @PostMapping("/inner")
     public ResponseEntity<ResponseTransferDto> createInnerTransfer(@RequestBody InnerTransferReqDto dto) {
-        return ResponseEntity.ok(transferService.createInnerTransfer(dto));
+        return ResponseEntity.status(202).body(transferService.createInnerTransfer(dto));
+    }
+
+    @GetMapping("/inner")
+    public String getInnerTransfer(Model model, Authentication authentication) {
+        model.addAttribute("user", accountService.getAccountInfo(getUsername(authentication)));
+        return "inner-transfer";
+    }
+
+    private String getUsername(Authentication authentication) {
+        AccountUserDetails principal = (AccountUserDetails) authentication.getPrincipal();
+        return principal.getUsername();
     }
 }
